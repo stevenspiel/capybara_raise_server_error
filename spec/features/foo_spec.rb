2102220f083c0890b3b825544d068e4e3ded4866
Steven Spiel <stevenspiel@gmail.com>
2016-10-20 15:55:17 -0400

    run test with js

--- spec/features/foo_spec.rb
+++ spec/features/foo_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "the signin process", :type => :feature do
+describe "the signin process", type: :feature, js: true do
   it "signs me in" do
     visit '/'
     expect(page).to have_content 'Success'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
-# Capybara.javascript_driver = :selenium
+Capybara.javascript_driver = :selenium
 Capybara.raise_server_errors = true
 # Add additional requires below this line. Rails is not loaded until this point!
 
